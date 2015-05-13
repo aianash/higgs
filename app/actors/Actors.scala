450bb27abbc4f73ca062dc1be4fa4ab4793fd4f7
Kumar Ishan <kumar.ishan89@gmail.com>
2015-05-13 22:11:18 +0530

    add: Search rest apis, json combinators and structures

--- app/actors/Actors.scala
+++ app/actors/Actors.scala
@@ -10,6 +10,8 @@ import com.twitter.finagle.builder.ClientBuilder
 import com.twitter.finagle.thrift.ThriftClientFramedCodecFactory
 
 import com.goshoplane.neutrino.service._
+import com.goshoplane.creed.service._
+import com.goshoplane.cassie.service._
 
 
 /**
@@ -26,6 +28,7 @@ object Actors {
   def userClient(implicit app: Application) = actors.userClient
   def bucketClient(implicit app: Application) = actors.bucketClient
   def shopPlanClient(implicit app: Application) = actors.shopPlanClient
+  def searchClient(implicit app: Application) = actors.searchClient
 }
 
 
@@ -57,10 +60,46 @@ class Actors(app: Application) extends Plugin {
     new Neutrino$FinagleClient(client, protocol)
   }
 
+  private val cassie = {
+    val protocol = new TBinaryProtocol.Factory()
+
+    val endpoint =
+      (for {
+        host <- app.configuration.getString("cassie.host")
+        port <- app.configuration.getInt("cassie.port")
+      } yield host + ":" + port) getOrElse "127.0.0.1:2424" // default endpoint
+
+    val connectionLimit = app.configuration.getInt("cassie.connection-limit").getOrElse(2)
+
+    val client = ClientBuilder().codec(new ThriftClientFramedCodecFactory(None, false, protocol))
+      .dest(endpoint).hostConnectionLimit(connectionLimit).build()
+
+    new Cassie$FinagleClient(client, protocol)
+  }
+
+  private val creed = {
+    val protocol = new TBinaryProtocol.Factory()
+
+    val endpoint =
+      (for {
+        host <- app.configuration.getString("creed.host")
+        port <- app.configuration.getInt("creed.port")
+      } yield host + ":" + port) getOrElse "127.0.0.1:2424" // default endpoint
+
+    val connectionLimit = app.configuration.getInt("creed.connection-limit").getOrElse(2)
+
+    val client = ClientBuilder().codec(new ThriftClientFramedCodecFactory(None, false, protocol))
+      .dest(endpoint).hostConnectionLimit(connectionLimit).build()
+
+    new Creed$FinagleClient(client, protocol)
+  }
+
+
   // lazily create a the feed client actor
   private lazy val feedClient   = system.actorOf(FeedClient.props(neutrino), "feedClient")
   private lazy val authService  = system.actorOf(AuthService.props(neutrino), "authService")
   private lazy val userClient   = system.actorOf(UserClient.props(neutrino), "userClient")
   private lazy val bucketClient = system.actorOf(BucketClient.props(neutrino), "bucketClient")
   private lazy val shopPlanClient = system.actorOf(ShopPlanClient.props(neutrino), "bucketClient")
+  private lazy val searchClient = system.actorOf(SearchClient.props(creed, cassie), "searchClient")
 }
